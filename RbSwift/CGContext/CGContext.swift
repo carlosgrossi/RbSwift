2450f801b744a1129b4e8cf497a82989e72921ab
Carlos Grossi <carlos.grossi@gmail.com>
2017-06-16 19:34:18 -0300

    add CGContext extension

---
+++ RbSwift/CGContext/CGContext.swift
@@ -0,0 +1,23 @@
+//
+//  CGContext.swift
+//  RbSwift
+//
+//  Created by Carlos Grossi on 16/06/17.
+//  Copyright © 2017 draveness. All rights reserved.
+//
+
+import CoreGraphics
+
+extension CGContext {
+	
+	public static func contextWith(cgImage: CGImage, size: CGSize, interpolationQuality: CGInterpolationQuality = .high) -> CGContext? {
+		let colorSpace: CGColorSpace = CGColorSpaceCreateDeviceRGB()
+		let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+		
+		let context = CGContext(data: nil, width: Int(size.width), height: Int(size.height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
+		context?.interpolationQuality = interpolationQuality
+		context?.draw(cgImage, in: CGRect(origin: CGPoint.zero, size: CGSize(width: CGFloat(size.width), height: CGFloat(size.height))))
+		return context
+	}
+	
+}
